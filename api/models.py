64111e3396ebc978c43497a281fc2d1ace1a310d
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-05-25 10:13:07 +0200

    New model - HotelModel

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class HotelModel(models.Model):
+    hotel_name = models.CharField(max_length=32, unique=True, blank=False, null=False)
+    address = models.CharField(max_length=128, blank=False, null=False)
+    rate = models.DecimalField(max_digits=4, decimal_places=2, blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.hotel_name
+    
+    def __repr__(self) -> str:
+        return self.__str__()
