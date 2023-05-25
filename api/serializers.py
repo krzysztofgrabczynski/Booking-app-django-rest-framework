897f8e110b711ea40514cb72e25b31da5b5d702b
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-05-25 10:48:27 +0200

    ModelSerializers for 'HotelModel' and 'HotelRoomModel'

--- api/serializers.py
+++ api/serializers.py
@@ -1,3 +1,25 @@
 from rest_framework.serializers import ModelSerializer
 
 from .models import HotelModel, HotelRoomModel
+
+
+class HotelSerializer(ModelSerializer):
+    class Meta:
+        model = HotelModel
+        fields = ["id", "hotel_name", "address", "rate",]
+
+
+class HotelRoomModel(ModelSerializer):
+    hotel = HotelSerializer(many=False)
+
+    class Meta:
+        model = HotelRoomModel
+        fields = [
+            "id",
+            "hotel",
+            "room_no",
+            "capacity",
+            "description",
+            "price_per_night",
+            "is_available",
+        ]
