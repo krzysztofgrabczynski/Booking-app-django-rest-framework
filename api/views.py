1f14f888623a24d277843e1e3cde56225dfdd713
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-05-25 10:57:53 +0200

    Initialization of the empty classes ModelViewSets for 'HotelModel' and 'HotelRoomModel'

--- api/views.py
+++ api/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+
+class HotelViewSet(viewsets.ModelViewSet):
+    pass
+
+
+class HotelRoomViewSet(viewsets.ModelViewSet):
+    pass
+
