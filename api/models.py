570713e10a274e27590344d42366973cf6a758a3
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-05-25 10:42:23 +0200

    models.py code refactor

--- api/models.py
+++ api/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class HotelModel(models.Model):
-    hotel_name = models.CharField(max_length=32, unique=True, blank=False, null=False)
-    address = models.CharField(max_length=128, blank=False, null=False)
+    hotel_name = models.CharField(max_length=32, unique=True)
+    address = models.CharField(max_length=128)
     rate = models.DecimalField(max_digits=4, decimal_places=2, blank=True, null=True)
 
     def __str__(self) -> str:
@@ -11,3 +11,23 @@ class HotelModel(models.Model):
 
     def __repr__(self) -> str:
         return self.__str__()
+
+
+class HotelRoomModel(models.Model):
+    hotel = models.ForeignKey(
+        HotelModel, on_delete=models.CASCADE, related_name="hotel_room"
+    )
+    room_no = models.SmallIntegerField()
+    capacity = models.PositiveSmallIntegerField()
+    description = models.TextField(default="")
+    price_per_night = models.PositiveIntegerField()
+    is_available = models.BooleanField(default=True)
+
+    def _name(self) -> str:
+        return f"{self.hotel.hotel_name} - room: {self.room_no}"
+
+    def __str__(self) -> str:
+        return self._name()
+
+    def __repr__(self) -> str:
+        return self._name()
