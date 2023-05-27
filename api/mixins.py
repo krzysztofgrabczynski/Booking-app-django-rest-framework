f82f1e9bdba300f97b12b873b81207935c46a420
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-05-27 17:15:29 +0200

    Craeting mixins permissions for easiest future development

---
+++ api/mixins.py
@@ -0,0 +1,10 @@
+from rest_framework import mixins
+
+from .permissions import AddHotelPermissions, IsAdminPermissions
+
+
+class AddHotelPermissionsMixin():
+    permission_classes = [AddHotelPermissions]
+
+class IsAdminPermissionsMixin():
+    permission_classes = [IsAdminPermissions]
