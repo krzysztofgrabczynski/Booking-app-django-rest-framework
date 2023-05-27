9c1ae7ce187dd1c51b8a20dafed56da28f1b55d1
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-05-27 13:11:30 +0200

    Updating views for HotelModel

--- api/views.py
+++ api/views.py
@@ -1,16 +1,39 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics, mixins, status
 from rest_framework.response import Response
 
 from .models import HotelModel, HotelRoomModel
 from .serializers import HotelSerializer, HotelRoomSerializer, HotelRoomCreationSerializer
 
 
-class HotelViewSet(viewsets.ModelViewSet):
+class HotelListRetriveGenericViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
     """
-    A viewset that provides information about hotels.
+    This is a generic viewset for list and retrive for HotelModel objects.
     """
-    serializer_class = HotelSerializer
     queryset = HotelModel.objects.all()
+    serializer_class = HotelSerializer
+
+class HotelCreateView(generics.CreateAPIView):
+    """
+    This is a CreateAPIView for creating a new hotel.
+    """
+    queryset = HotelModel.objects.all()
+    serializer_class = HotelSerializer
+
+class HotelUpdateDestroyView(mixins.UpdateModelMixin, mixins.DestroyModelMixin, viewsets.GenericViewSet):
+    """
+    This is a generic viewset for updating and deleting existing hotel. Only for admin users.
+    """
+    queryset = HotelModel.objects.all()
+    serializer_class = HotelSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(f'Deleteing of the {instance} Hotel', status=status.HTTP_202_ACCEPTED)
+
+hotel_update_view = HotelUpdateDestroyView.as_view({'put': 'update'})
+hotel_delete_view = HotelUpdateDestroyView.as_view({'delete': 'destroy'})
+
 
 
 class HotelRoomViewSet(viewsets.ModelViewSet):
