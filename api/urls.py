a02e60d7a98f77328e807c7768f995c1ec4b5aca
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-05-27 13:12:08 +0200

    Changing HotelModel urls for new views

--- api/urls.py
+++ api/urls.py
@@ -4,9 +4,12 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-router.register(r'hotels', views.HotelViewSet)
+router.register(r'hotels', views.HotelListRetriveGenericViewSet)
 router.register(r'rooms', views.HotelRoomViewSet)
 
 urlpatterns = [
-    path("", include(router.urls))
+    path("", include(router.urls)),
+    path("create_hotel/", views.HotelCreateView.as_view()),
+    path("delete_hotel/<int:pk>/", views.hotel_delete_view),
+    path("update_hotel/<int:pk>/", views.hotel_update_view),
 ]
