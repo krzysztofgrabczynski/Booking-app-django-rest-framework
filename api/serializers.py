e37ae1f3e18519883ab8c03b62f119ba04de8600
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-05-27 13:10:54 +0200

    Update serializers for HotelModel - deleting information about hotel rooms

--- api/serializers.py
+++ api/serializers.py
@@ -3,33 +3,14 @@ from rest_framework.serializers import ModelSerializer
 from .models import HotelModel, HotelRoomModel
 
 
-class HotelRoomsSerializer(ModelSerializer):
-    """
-    This serializer is used to serialize data without information about hotel (for HotelSerializer).
-    """
-
-    class Meta:
-        model = HotelRoomModel
-        fields = [
-            "id",
-            "room_no",
-            "capacity",
-            "description",
-            "price_per_night",
-            "is_available",
-        ]
-
-
 class HotelSerializer(ModelSerializer):
     """
-    This is main serializer for HotelModel.
+    This is main serializer for HotelModel 
     """
-
-    hotel_room = HotelRoomsSerializer(many=True, required=False)
 
     class Meta:
         model = HotelModel
-        fields = ["id", "hotel_name", "address", "rate", "hotel_room"]
+        fields = ["id", "hotel_name", "address", "rate"]
 
 
 class HotelMiniInformationSerializer(ModelSerializer):
