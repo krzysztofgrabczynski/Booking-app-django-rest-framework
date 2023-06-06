44c832b0ac65e10b68b8127f6e9f366af44eab76
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-06-06 21:00:09 +0200

    New urls for user detail and user reservation detail

--- api/urls.py
+++ api/urls.py
@@ -10,6 +10,12 @@ router.register(r"rooms", views.HotelRoomViewSet, basename="room")
 urlpatterns = [
     path("", include(router.urls)),
     path("sign_up/", views.UserCreateView.as_view()),
+    path("profile/<int:pk>/", views.UserDetailView.as_view()),
+    path(
+        "profile/<int:pk>/reservations/",
+        views.UserDetailView.as_view(),
+        {"action": "reservations"},
+    ),
     path("create_hotel/", views.HotelCreateView.as_view()),
     path("delete_hotel/<int:pk>/", views.hotel_delete_view),
     path("update_hotel/<int:pk>/", views.hotel_update_view),
