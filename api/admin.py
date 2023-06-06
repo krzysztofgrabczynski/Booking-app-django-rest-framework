9a42ed5b9d5edbc567fecba2a5397b5e2d2a3a1b
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-06-06 20:57:57 +0200

    Changing display list about Users on the admin page

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
-from .models import HotelModel, HotelRoomModel
+from .models import HotelModel, HotelRoomModel, RoomReservation
 
 
 admin.site.register(HotelModel)
 admin.site.register(HotelRoomModel)
+admin.site.register(RoomReservation)
+
+admin.site.unregister(User)
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ["id", "username", "email"]
